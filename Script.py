67bb188bb13dc6755ec7f8b8837a33190614b961
Manu6373 <manud8861@gmail.com>
2025-08-23 16:35:17 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -216,7 +216,7 @@ Nᴀᴍᴇ : {}
 
 Mᴇꜱꜱᴀɢᴇ : <b>{}</b>"""
     
-    CAPTION = """<b><a href="https://t.me/McMoviesKA">{file_name}</a></b>\n\n<b>⚜️ Powered By : <a href="https://t.me/McMoviesKA">[ @McMoviesKA ]</a></b>"""
+    CAPTION = """<b><a href="https://t.me/McMoviesKA">{file_name}</a></b>\n\n<b>⚜️ For More Movies Join Here : <a href="https://t.me/McMoviesKA">[ @McMoviesKA ]</a></b>"""
 
     
     MOVIE_UPDATE_NOTIFY_TXT = """
