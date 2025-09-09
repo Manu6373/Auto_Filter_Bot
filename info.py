3829f525a5adb4a6bd84a80e2dcb6014fd0f9427
Manu6373 <manud8861@gmail.com>
2025-09-09 22:48:11 +0530

    Update info.py

--- info.py
+++ info.py
@@ -127,9 +127,9 @@ UPDATE_CHNL_LNK = environ.get('UPDATE_CHNL_LNK', 'https://t.me/McMoviesKA') # Up
 # ============================
 # User Configuration
 # ============================
-auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '').split()]
+auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '1408101293').split()]
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
-PREMIUM_USER = [int(user) if id_pattern.search(user) else user for user in environ.get('PREMIUM_USER', '').split()]
+PREMIUM_USER = [int(user) if id_pattern.search(user) else user for user in environ.get('PREMIUM_USER', '1408101293').split()]
 
 # ============================
 # Miscellaneous Configuration
