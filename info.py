55bb1906ee3f1d1c3f25fa43bb9893304609fae4
Manu6373 <manud8861@gmail.com>
2026-01-23 19:37:14 +0530

    Update info.py

--- info.py
+++ info.py
@@ -60,7 +60,7 @@ auth_channels     = environ.get("AUTH_CHANNELS", "")# Channels for force sub (ma
 # Payment Configuration
 # ============================
 QR_CODE = environ.get('QR_CODE', 'https://graph.org/file/e419f801841c2ee3db0fc.jpg')    # QR code image for payments
-OWNER_UPI_ID = environ.get('OWNER_UPI_ID', 'ɴᴏ ᴀᴠᴀɪʟᴀʙʟᴇ ʀɪɢʜᴛ ɴᴏᴡ')    # Owner UPI ID for payments
+OWNER_UPI_ID = environ.get('OWNER_UPI_ID', 'captainro45@ybl')    # Owner UPI ID for payments
 
 STAR_PREMIUM_PLANS = {
     10: "7day",
